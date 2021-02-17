623b828be3847a93ba4b42c99ad992888cff025e
Pavel <imran.pavel@gmail.com>
2021-02-17 15:37:32 +0200

    Here we go.

---
+++ actions/actions.py
@@ -0,0 +1,40 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import asyncio
+import aiohttp
+
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+class ActionShowLibrary(Action):
+  
+  def name(self) -> Text:
+    return "action_show_library"
+
+  def run(self, dispatcher, tracker, domain) -> List[Dict[Text, Any]]:
+    print('here came')
+    dispatcher.utter_message(text="Will see")
+    return []
